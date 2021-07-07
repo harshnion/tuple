008ef16bb86c09ae654e3c82c8cc708cd3801213
harshnion <86431354+harshnion@users.noreply.github.com>
2021-07-07 23:30:27 +0530

    13_commit

---
+++ Tuples.py
@@ -0,0 +1,11 @@
+# today we are here to learn about new method of entering data 
+# i.e.tuples
+# These are fixed type of data once we enter a data in tuple format it cannot be changed again
+
+# so lets start something with tuple
+Weak_Days=('Monday','Tuesday','Wednesday','Thruday','Friday','Saturday','Sunday')
+print(Weak_Days)
+
+# Now try to add something in it
+
+
